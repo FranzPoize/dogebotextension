a8272b353993134624c62698854f87975f8de814
FranzPoize <francois.poizat@gmail.com>
2013-12-20 16:52:52 +0100

    second commit

--- dogetip.js
+++ dogetip.js
@@ -1,59 +1,91 @@
 var modHash;
 
-$.get('https://ssl.reddit.com/api/me.json').success(function(data) {
-	modHash = data.data.modhash;
-});
+
 
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-$('.comment .entry .buttons').each(function() {
+function doDogeTiping() {
 	var userLink = $(this).parent().find('.tagline .author').attr('href'),
-		fullname = $(this).parent().parent().parent().attr('data-fullname');
+		fullname = $(this).parent().parent().parent().attr('data-fullname') || $(this).parent().parent().attr('data-fullname');
 	if (userLink) {
 		var urlArray = userLink.split('/'),
 			username = urlArray[urlArray.length-1],
-			dogetip = $('<li><a class="dogetip" style="cursor:pointer">dogetip</a></li>');
+			dogetip = $('<li><a class="dogetip doge-tip" style="cursor:pointer">dogetip</a></li>');
 
 		$(this).append(dogetip);
 
 		dogetip.click(function() {
-			var element = $('<div class="dogetip-popup" style="position:absolute;"><input type="text" placeholder="Such amount !"></input>&nbsp;<img src="'+
-				chrome.extension.getURL("images/dogecoin.png")+
-				'" />&nbsp;<button class="send-doge-coin">send</button></div>')
-				.css({left:this.offsetLeft+this.clientWidth,top:this.offsetTop-18}),
+			$('.dogetip-popup').remove();
+			var element = $('<div class="dogetip-popup doge-tip" style="position:absolute;">'+
+					'<div class="dogetip-popup-before"></div>'+
+					'<div class="dogetip-popup-after"></div>'+
+					'<div class="doge-header">'+
+						'<img src="'+chrome.extension.getURL("images/dogecoin.png")+'" />'+
+					'</div>'+
+					'<div class="doge-body">'+
+						'<input class="doge-amount doge-tip" type="text" placeholder="Such amount !"></input>'+
+						'&nbsp;<button class="send-doge-coin doge-tip">Send</button>'+
+					'</div>'+
+				'</div>')
+				.css({left:this.offsetLeft+this.clientWidth+25,top:this.offsetTop-18}),
 				input = $(element).find('input');
 			$('body').append(element);
 
-			input.focus().blur(function() {
-				setTimeout(function() {
-					$(element).remove();
-				},200);
-			});
-
-			$(element).find('.send-doge-coin').click(function() {
-				if (isNumber(input.val())) {
-					$.ajax({
-						url:'https://ssl.reddit.com/api/comment.json',
-						type:'POST',
-						contentType:'application/json',
-						data: JSON.stringify({
-							api_type:"json",
-							text:"+/u/"+username+" "+input.val()+" doge",
-							parent:fullname,
-							uh:modHash
-						}),
-						headers: {
-							'X-Modhash':modHash,
-						}
-					}).success(function() {
-						console.log('success');
-					}).error(function() {
-						console.log('error');
-					})
+			input.focus()
+			$(document).click(function(e) {
+				if(!$(e.target).hasClass('doge-tip')) {
+					setTimeout(function() {
+						abortMoonMission()
+					},200);
 				}
 			});
+			var btn = $(element).find('.send-doge-coin');
+
+			function sendDogeCoin () {
+				if (isNumber(input.val())) {
+					$.get('http://www.reddit.com/api/me.json').success(function(data) {
+					modHash = data.data.modhash;
+					var form = $('<form>').
+							append($('<input>').attr({name:'api_type',value:'json'})).
+							append($('<input>').attr({name:'text',value:"+/u/dogetipbot "+input.val()+" doge"})).
+							append($('<input>').attr({name:'thing_id',value:fullname})).
+							append($('<input>').attr({name:'uh',value:modHash}))
+					$.ajax({
+						url:'http://www.reddit.com/api/comment',
+						type:'POST',
+						data: form.serialize()
+					}).success(function() {
+						btn.text('Sent').css('background-color','#33f572');
+						setTimeout(function() {
+							abortMoonMission()
+						},1000)
+					}).error(function() {
+						btn.text('Error').css('background-color','#f52323');
+						setTimeout(function() {
+							btn.text('Send').css('background-color','rgb(240, 240, 240)');
+						},1000)
+					})
+					});	
+				}
+			}
+
+			function abortMoonMission() {
+				$(element).remove();
+				$(document).off('keydown');
+			}
+			
+			$(document).on('keydown',function(e) {
+				if (e.keyCode == 27) {
+			       abortMoonMission()
+			    } else if (e.keyCode == 13) {
+			        sendDogeCoin()
+			    }
+			})
+			btn.click(sendDogeCoin);
+
 		})
 	}
-});
+}
+$('.content .entry .buttons').each(doDogeTiping);
